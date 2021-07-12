e8356250521db650c0a1c41fca36b25558a8c2e4
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-07-12 22:03:17 +0300

    Forgotten linter error

--- spec/requests/properties_request_spec.rb
+++ spec/requests/properties_request_spec.rb
@@ -9,12 +9,12 @@ RSpec.describe 'properties', type: :request do
     end
     it 'should not create property when there is no logged in user' do
       post '/api/v1/properties', params: {
-          name: Faker::Commerce.product_name,
-          address: Faker::Address.street_address,
-          monthly_price: rand(1.0..100.0),
-          is_for_rent: true,
-          description: Faker::Lorem.sentence(word_count: 3),
-          user_id: User.first.id
+        name: Faker::Commerce.product_name,
+        address: Faker::Address.street_address,
+        monthly_price: rand(1.0..100.0),
+        is_for_rent: true,
+        description: Faker::Lorem.sentence(word_count: 3),
+        user_id: User.first.id
       }
 
       expect(response).to have_http_status(:forbidden)
@@ -22,11 +22,11 @@ RSpec.describe 'properties', type: :request do
 
     it 'should create property when there is logged in user' do
       post '/api/v1/properties', params: {
-          name: Faker::Commerce.product_name,
-          address: Faker::Address.street_address,
-          monthly_price: rand(1.0..100.0),
-          is_for_rent: true,
-          description: Faker::Lorem.sentence(word_count: 3)
+        name: Faker::Commerce.product_name,
+        address: Faker::Address.street_address,
+        monthly_price: rand(1.0..100.0),
+        is_for_rent: true,
+        description: Faker::Lorem.sentence(word_count: 3)
       }, headers: { Authorization:
         JsonWebToken.encode(user_id: subject.id) }
 
