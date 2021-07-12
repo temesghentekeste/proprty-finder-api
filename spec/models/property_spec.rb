f205961968116307aa0266e5fda6bf5aa0477d94
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-07-12 12:24:33 +0300

    Add property spec; test the presence of property attributes

---
+++ spec/models/property_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Property, type: :model do
+  subject { Property.new }
+
+  describe 'validations' do
+    it { should validate_presence_of :user_id }
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :address }
+    it { should validate_presence_of :description }
+    it { should validate_presence_of :is_for_rent }
+    it { should validate_presence_of :monthly_price }
+
+  end
+end
