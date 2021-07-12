dd0af821ef0457d8e14ba77b396d7227dde4f0b3
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-07-12 12:36:34 +0300

    Add test for attaching default featured image to property model

--- spec/models/property_spec.rb
+++ spec/models/property_spec.rb
@@ -16,5 +16,18 @@ RSpec.describe Property, type: :model do
       it { should belong_to(:user) }
       it { should have_many(:favorites) }
     end
+
+    it 'attaches default featured image' do
+      @user = FactoryBot.create(:user)
+      @property = Property.create!(
+        name: Faker::Commerce.product_name,
+        address: Faker::Address.street_address,
+        monthly_price: rand(1.0..100.0),
+        is_for_rent: true,
+        description: Faker::Lorem.sentence(word_count: 3),
+        user_id: @user.id
+    )
+      expect(@property.featured_image).to be_attached
+    end
   end
 end
