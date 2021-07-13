2286ff515409ec180717e37aca569b4751be434b
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-07-13 13:18:07 +0300

    Add unit tests for favorites#create api endpoint

--- spec/requests/favorites_request_spec.rb
+++ spec/requests/favorites_request_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe 'favorites', type: :request do
+  @user = FactoryBot.create(:user)
+  @property = FactoryBot.create(:property)
+  @favorite = FactoryBot.create(:favorite)
+
+  favorite_params = { property_id: Property.last.id }
   context 'GET api/v1/favorites' do
     it 'should forbid getting all favorites for unathenticated user' do
       get '/api/v1/favorites'
@@ -8,17 +13,11 @@ RSpec.describe 'favorites', type: :request do
     end
 
     it 'should get all favorites for athenticated user' do
-      @user = FactoryBot.create(:user)
-
       get '/api/v1/favorites', headers: { Authorization: JsonWebToken.encode(user_id: User.first.id) }
       expect(response).to have_http_status(:ok)
     end
 
     it 'should have valid json format' do
-      @user = FactoryBot.create(:user)
-      @property = FactoryBot.create(:property)
-      @favorite = FactoryBot.create(:favorite)
-
       get '/api/v1/favorites', headers: { Authorization: JsonWebToken.encode(user_id: User.first.id) }
       expect(response).to have_http_status(:ok)
 
@@ -26,6 +25,43 @@ RSpec.describe 'favorites', type: :request do
       actual = json_response.dig(:data, 0, :type)
       expected = 'favorite'
       expect(actual).to eq(expected)
+    end
+  end
+
+  context 'POST api/v1/favorites' do
+    it 'should not create favorite when there is no logged in user' do
+      post '/api/v1/favorites', params: favorite_params
+
+      expect(response).to have_http_status(:forbidden)
+    end
+
+    it 'should create favorite when there is logged in user' do
+      post '/api/v1/favorites', params: favorite_params, headers: { Authorization:
+        JsonWebToken.encode(user_id: User.last.id) }
+
+      expect(response).to have_http_status(:created)
+    end
+
+    it 'should increast favorite count by 1 when created' do
+      count = Favorite.all.size
+
+      post '/api/v1/favorites', params: favorite_params, headers: { Authorization:
+        JsonWebToken.encode(user_id: User.last.id) }
+
+      expect(response).to have_http_status(:created)
+      expect(Favorite.all.size).to eq(count + 1)
+    end
+
+    it 'should return valid json when favorite property is created' do
+      post '/api/v1/favorites', params: favorite_params, headers: { Authorization:
+        JsonWebToken.encode(user_id: User.last.id) }
+
+      expect(response).to have_http_status(:created)
+
+      json_response = JSON.parse(response.body, symbolize_names: true)
+      expect(json_response).to eq({
+                                    success: 'Added to your favorite list successfully!'
+                                  })
     end
   end
 end
