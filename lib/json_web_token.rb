efccd088c8f9b6574e6d4547161735a64c90e0c2
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-07-14 22:39:06 +0300

    Trace prod. error related to jwt

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,13 +1,18 @@
 class JsonWebToken
   SECRET_KEY = Rails.application.secrets.secret_key_base || ENV['SECRET_KEY_BASE']
-
+  secret_key_base = SECRET_KEY[0]
+  puts "******************************"
+  p SECRET_KEY
+  puts "******************************"
+  p secret_key_base
+  puts "******************************"
   def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
-    JWT.encode(payload, SECRET_KEY)
+    JWT.encode(payload, secret_key_base)
   end
 
   def self.decode(token)
-    decoded = JWT.decode(token, SECRET_KEY).first
+    decoded = JWT.decode(token, secret_key_base).first
     HashWithIndifferentAccess.new decoded
   end
 end
