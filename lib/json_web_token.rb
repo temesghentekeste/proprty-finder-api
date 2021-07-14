06eac89720b1f5f88e7f62e36d5ba73b20f32f93
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-07-14 23:11:38 +0300

    Raise local variale to instance variable

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,21 +1,21 @@
 class JsonWebToken
   SECRET_KEY = Rails.application.secrets.secret_key_base || ENV['SECRET_KEY_BASE']
-  secret_key_base = SECRET_KEY[0]
+  @secret_key_base = SECRET_KEY[0]
   puts "******************************"
-  p SECRET_KEY
+  p SECRET_@EY
   puts "******************************"
-  p secret_key_base
+  p @secret_key_base
   puts "******************************"
   def self.encode(payload, exp = 24.hours.from_now)
     puts "**********************************************"
     puts "Trying to generate token"
     puts "**********************************************"
     payload[:exp] = exp.to_i
-    JWT.encode(payload, secret_key_base)
+    JWT.encode(payload, @secret_key_base)
   end
 
   def self.decode(token)
-    decoded = JWT.decode(token, secret_key_base).first
+    decoded = JWT.decode(token, @secret_key_base).first
     HashWithIndifferentAccess.new decoded
   end
 end
